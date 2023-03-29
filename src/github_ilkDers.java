baa8a33db43595cbc5d214515587385258db516c
EmreDereli1 <emredereliii123@gmail.com>
2023-03-29 21:26:19 +0200

    230327

--- src/github_ilkDers.java
+++ src/github_ilkDers.java
@@ -2,5 +2,7 @@ public class github_ilkDers {
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
+        System.out.println("=====================");
+        System.out.println("have a wise day");
     }
 }
